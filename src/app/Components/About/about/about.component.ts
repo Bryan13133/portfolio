e52e9b3d2d100244211beab1becb81d47b34bb9c
Bryan13133 <52942708+Bryan13133@users.noreply.github.com>
2020-08-25 20:52:33 -0600

    About

--- src/app/Components/About/about/about.component.ts
+++ src/app/Components/About/about/about.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 @Component({
   selector: 'app-about',
@@ -8,8 +10,20 @@ import { Component, OnInit } from '@angular/core';
 export class AboutComponent implements OnInit {
 
   constructor() { }
-
+  programd:any;
   ngOnInit(): void {
+    AOS.init();
   }
+
+  programs = [
+    {name: 'CSS',percentage:'60%'},
+    {name: 'HTML',percentage:'75%'},
+    {name: 'JavaScript',percentage:'70%'},
+    {name: 'Angular',percentage:'75%'},
+    {name: 'Java',percentage:'80%'},
+    {name: 'C#',percentage:'70%'},
+    {name: 'Node.js',percentage:'50%'},
+    {name: 'Objective-C',percentage:'35%'}
+  ]
 
 }
